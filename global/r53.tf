b68bd5ed3adf0630d75160294d529f095e4162b9
dornera <dornera@mit.edu>
2018-11-19 08:38:27 -0500

    Configure Route53 private and public zones

--- global/r53.tf
+++ global/r53.tf
@@ -4,15 +4,28 @@ resource "aws_route53_zone" "main_pub" {
   comment = "Public DNS Zone"
 }
 
-/*
-#Create the Private mitlib.net Zone
+module "prodvpc" {
+  source    = "git::https://github.com/mitlibraries/tf-mod-shared-provider?ref=master"
+  workspace = "prod"
+}
+
+#Create the Private mitlib.net Zone and associate Prod VPC
 resource "aws_route53_zone" "main_priv" {
   name    = "mitlib.net"
   comment = "Internal DNS"
 
   vpc {
-    vpc_id = "${aws_vpc.example.id}"
+    vpc_id = "${module.prodvpc.vpc_id}"
   }
 }
-*/
 
+#Associate Stage VPC with Internal DNS
+module "stagevpc" {
+  source    = "git::https://github.com/mitlibraries/tf-mod-shared-provider?ref=master"
+  workspace = "stage"
+}
+
+resource "aws_route53_zone_association" "stage" {
+  zone_id = "${aws_route53_zone.main_priv.zone_id}"
+  vpc_id  = "${module.stagevpc.vpc_id}"
+}
