2e4739c57979707e3e9b1efa736ac1024b6eb66a
dornera <dornera@mit.edu>
2018-11-28 12:45:55 -0500

    Add TF Config for grandchallenges CDN + S3

---
+++ apps/grandchallenges/grandchallenges.tf
@@ -0,0 +1,21 @@
+#The certificate was created manually (via MIT) and imported manually
+#We will investigate options for generating SSL certificates in the future
+
+module "grand_web" {
+  source               = "git::https://github.com/mitlibraries/tf-mod-cdn-s3?ref=initial"
+  name                 = "grandchallenges"
+  aliases              = ["grandchallenges.mitlib.net"]
+  ext_aliases          = ["grandchallenges.mit.edu"]
+  parent_zone_name     = "mitlib.net"
+  cors_allowed_origins = ["grandchallenges.mit.edu"]
+
+  custom_error_response = [
+    {
+      error_code         = "404"
+      response_code      = "200"
+      response_page_path = "/index.html"
+    },
+  ]
+
+  acm_certificate_arn = "arn:aws:acm:us-east-1:672626379771:certificate/cdb62536-c66f-4eff-940a-d90ecc869b0b"
+}
