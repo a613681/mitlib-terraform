655e500598b2c0bdce3c3d5367b828e7b8c22014
dornera <dornera@mit.edu>
2019-01-25 11:04:52 -0500

    Adds outputs for policy arns

--- shared/elasticsearch/outputs.tf
+++ shared/elasticsearch/outputs.tf
@@ -7,3 +7,13 @@ output "es_endpoint" {
   description = "Domain-specific endpoint used to submit index, search, and data upload requests"
   value       = "${module.elasticsearch.endpoint}"
 }
+
+output "read_policy_arn" {
+  description = "Default domain read only policy ARN"
+  value       = "${module.elasticsearch.read_policy_arn}"
+}
+
+output "write_policy_arn" {
+  description = "Default domain write policy ARN"
+  value       = "${module.elasticsearch.write_policy_arn}"
+}
