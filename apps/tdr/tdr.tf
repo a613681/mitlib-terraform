e1f3ab1ebcdaf9170d7d10dc94d883e9f84508bb
dornera <dornera@mit.edu>
2019-02-22 12:33:28 -0500

    Add TDR app

---
+++ apps/tdr/tdr.tf
@@ -0,0 +1,23 @@
+resource "aws_route53_record" "tdr" {
+  name    = "tdr"
+  ttl     = 300
+  type    = "CNAME"
+  zone_id = "${module.shared.public_zoneid}"
+  records = ["mitlib-trac.pantheonsite.io"]
+}
+
+resource "aws_route53_record" "tdr_dev" {
+  name    = "tdr-dev"
+  ttl     = 300
+  type    = "CNAME"
+  zone_id = "${module.shared.public_zoneid}"
+  records = ["dev-mitlib-trac.pantheonsite.io"]
+}
+
+resource "aws_route53_record" "tdr_test" {
+  name    = "tdr-test"
+  ttl     = 300
+  type    = "CNAME"
+  zone_id = "${module.shared.public_zoneid}"
+  records = ["test-mitlib-trac.pantheonsite.io"]
+}
