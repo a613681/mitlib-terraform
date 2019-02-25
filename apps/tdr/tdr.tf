82f31d278ed7015549f0357b4a92a24eff19c73e
dornera <dornera@mit.edu>
2019-02-25 15:33:56 -0500

    Updates DNS to correct CNAME for live site

--- apps/tdr/tdr.tf
+++ apps/tdr/tdr.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "tdr" {
   ttl     = 300
   type    = "CNAME"
   zone_id = "${module.shared.public_zoneid}"
-  records = ["mitlib-trac.pantheonsite.io"]
+  records = ["live-mitlib-trac.pantheonsite.io"]
 }
 
 resource "aws_route53_record" "tdr_dev" {
