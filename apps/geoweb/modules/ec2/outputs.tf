73b82ef5489596ecb14d4a3935bfd758ec7675ec
Mike Graves <mgraves@mit.edu>
2019-08-07 11:05:14 -0400

    Add EFS backups
    This adds AWS backups for the Solr and GeoServer EFS mounts.

--- apps/geoweb/modules/ec2/outputs.tf
+++ apps/geoweb/modules/ec2/outputs.tf
@@ -2,3 +2,8 @@ output "fqdn" {
   value       = "${aws_route53_record.default.fqdn}"
   description = "Route 53 domain name"
 }
+
+output "efs" {
+  value       = "${aws_efs_file_system.default.arn}"
+  description = "EFS ARN"
+}
