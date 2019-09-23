674a8e73831f0b950635a7668582de110736066c
Mike Graves <mgraves@mit.edu>
2019-09-23 14:47:35 -0400

    Upgrade global config to 0.12
    There are a few minor changes that get pulled in with the newer version
    of the S3 tfstate backend.

--- global/outputs.tf
+++ global/outputs.tf
@@ -1,34 +1,35 @@
 output "public_zoneid" {
   description = "Route53 Public Zone ID"
-  value       = "${aws_route53_zone.main_pub.zone_id}"
+  value       = aws_route53_zone.main_pub.zone_id
 }
 
 output "public_zonename" {
   description = "Route53 Public Zone name"
-  value       = "${aws_route53_zone.main_pub.name}"
+  value       = aws_route53_zone.main_pub.name
 }
 
 output "private_zoneid" {
   description = "Route53 Private Zone ID"
-  value       = "${aws_route53_zone.main_priv.zone_id}"
+  value       = aws_route53_zone.main_priv.zone_id
 }
 
 output "private_zonename" {
   description = "Route53 Private Zone name"
-  value       = "${aws_route53_zone.main_priv.name}"
+  value       = aws_route53_zone.main_priv.name
 }
 
 output "mitlib_cert" {
   description = "*.mitlib.net wildcard certificate"
-  value       = "${module.acm_request_certificate.arn}"
+  value       = module.acm_request_certificate.arn
 }
 
 output "mit_saml_arn" {
   description = "MIT Identity provider arn (SAML Federated login)"
-  value       = "${aws_iam_saml_provider.mit.arn}"
+  value       = aws_iam_saml_provider.mit.arn
 }
 
 output "docsvcs_beanstalk_name" {
   description = "Name of Docsvcs Elastic Beanstalk application"
-  value       = "${aws_elastic_beanstalk_application.docsvcs.name}"
+  value       = aws_elastic_beanstalk_application.docsvcs.name
 }
+
--- global/r53.tf
+++ global/r53.tf
@@ -5,7 +5,7 @@ resource "aws_route53_zone" "main_pub" {
 }
 
 module "prodvpc" {
-  source    = "github.com/mitlibraries/tf-mod-shared-provider?ref=0.11"
+  source    = "github.com/mitlibraries/tf-mod-shared-provider?ref=0.12"
   workspace = "prod"
 }
 
@@ -15,21 +15,22 @@ resource "aws_route53_zone" "main_priv" {
   comment = "Internal DNS"
 
   vpc {
-    vpc_id = "${module.prodvpc.vpc_id}"
+    vpc_id = module.prodvpc.vpc_id
   }
 
   lifecycle {
-    ignore_changes = ["vpc"]
+    ignore_changes = [vpc]
   }
 }
 
 #Associate Stage VPC with Internal DNS
 module "stagevpc" {
-  source    = "github.com/mitlibraries/tf-mod-shared-provider?ref=0.11"
+  source    = "github.com/mitlibraries/tf-mod-shared-provider?ref=0.12"
   workspace = "stage"
 }
 
 resource "aws_route53_zone_association" "stage" {
-  zone_id = "${aws_route53_zone.main_priv.zone_id}"
-  vpc_id  = "${module.stagevpc.vpc_id}"
+  zone_id = aws_route53_zone.main_priv.zone_id
+  vpc_id  = module.stagevpc.vpc_id
 }
+
