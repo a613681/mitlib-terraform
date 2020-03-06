6f98605dba278bd84c0eb4a233a9d8dc93980861
Elda Sotiri <elda@libm-0517046.mit.edu>
2020-03-06 11:45:46 -0500

    tarot upgrade tf_v.12

--- apps/tarot/main.tf
+++ apps/tarot/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 terraform {
-  required_version = ">= 0.11.10"
+  required_version = ">= 0.12"
 
   backend "s3" {
     region         = "us-east-1"
@@ -16,5 +16,5 @@ terraform {
 }
 
 module "shared" {
-  source = "github.com/mitlibraries/tf-mod-shared-provider?ref=0.11"
+  source = "github.com/mitlibraries/tf-mod-shared-provider?ref=0.12"
 }
