52f41516bc0eca77a275833cce685a90e049f395
eldamit <61520319+eldamit@users.noreply.github.com>
2020-05-06 19:44:07 -0400

    create alb and update outputs

---
+++ shared/network/albMITnet.tf
@@ -0,0 +1,97 @@
+#######################
+### Restricted ALB ###
+######################
+#Creates default restricted (18.0.0.0/9) ALB that uses *.mitlib.net cert by default
+module "mitnet_alb_restricted" {
+  source                    = "github.com/mitlibraries/tf-mod-alb?ref=0.11"
+  # name                      = "${module.label.name}-alb-restricted"
+  name                      = "mitnet-alb-restricted"
+  http_ingress_cidr_blocks  = ["${var.alb_cidrs}"]
+  https_ingress_cidr_blocks = ["${var.alb_cidrs}"]
+  vpc_id = var.vpc_id
+  # vpc_id                    = "${module.vpc.vpc_id}"
+  ip_address_type           = "ipv4"
+  subnet_ids                = var.public_subnets
+  # subnet_ids                = ["${module.vpc.public_subnets}"]
+  access_logs_enabled       = "false"
+  access_logs_region        = "${var.aws_region}"
+  https_enabled             = "true"
+  certificate_arn           = "${module.shared.mitlib_cert}"
+}
+
+module "sg_label" {
+  source = "github.com/mitlibraries/tf-mod-name?ref=0.11"
+  name   = "mitnet-alb-restricted"
+}
+
+# Create default security group to allow all ingress from restricted ALB
+module "mitnet_all_access_from_alb" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "2.13.0"
+
+  name        = "${module.mitnet_alb_restricted.alb_name} all Ingress"
+  description = "Allow all ingress from restricted ALB"
+  vpc_id      = var.vpc_id
+  # vpc_id      = "${module.shared.vpc_id}" 
+
+  computed_ingress_with_source_security_group_id = [
+    {
+      from_port                = 0
+      to_port                  = 0
+      protocol                 = "-1"
+      source_security_group_id = "${module.mitnet_alb_restricted.security_group_id}"
+    },
+  ]
+
+  number_of_computed_ingress_with_source_security_group_id = 1
+  tags                                                     = "${module.sg_label.tags}"
+}
+
+#########################
+### Public Facing ALB ###
+#########################
+#Creates default public ALB that uses *.mitlib.net cert by default
+module "mitnet_alb_public" {
+  source              = "github.com/mitlibraries/tf-mod-alb?ref=0.11"
+  name                = "mitnet-alb-public"
+  vpc_id              = var.vpc_id
+  # vpc_id              = "${module.vpc.vpc_id}"
+  ip_address_type     = "ipv4"
+  subnet_ids                = var.public_subnets
+  # subnet_ids          = ["${module.vpc.public_subnets}"]
+  access_logs_enabled = "false"
+  access_logs_region  = "${var.aws_region}"
+  https_enabled       = "true"
+  certificate_arn     = "${module.shared.mitlib_cert}"
+}
+
+module "sg_label_public" {
+  source = "github.com/mitlibraries/tf-mod-name?ref=0.11"
+  name   = "mitnet-alb-public"
+}
+
+# Create default security group to allow all ingress from public ALB
+
+module "all_access_from_mitnet_alb_public" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "2.13.0"
+
+  name        = "${module.mitnet_alb_public.alb_name} all Ingress"
+  description = "Allow all ingress from restricted ALB"
+  vpc_id              = var.vpc_id
+  # vpc_id      = "${module.shared.vpc_id}"
+
+  computed_ingress_with_source_security_group_id = [
+    {
+      from_port                = 0
+      to_port                  = 0
+      protocol                 = "-1"
+      source_security_group_id = "${module.mitnet_alb_public.security_group_id}"
+    },
+  ]
+
+  number_of_computed_ingress_with_source_security_group_id = 1
+  tags                                                     = "${module.sg_label_public.tags}"
+}
+
+
--- shared/network/outputs.tf
+++ shared/network/outputs.tf
@@ -128,3 +128,108 @@ output "alb_public_zone_id" {
   value       = module.alb_public.alb_zone_id
 }
 
+##############################
+##### MITnet ALB OUTPUTS #####
+##############################
+
+# MITnet Restricted
+output "mitnet_alb_restricted_arn" {
+  description = "Restricted ALB arn"
+  value       = module.mitnet_alb_restricted.alb_arn
+}
+
+output "mitnet_alb_restricted_arn_suffix" {
+  description = "The ARN suffix of the ALB"
+  value       = module.mitnet_alb_restricted.alb_arn_suffix
+}
+
+output "mitnet_alb_restricted_name" {
+  description = "Restricted ALB name"
+  value       = module.mitnet_alb_restricted.alb_name
+}
+
+output "mitnet_alb_restricted_dnsname" {
+  description = "Restricted ALB DNS name"
+  value       = module.mitnet_alb_restricted.alb_dns_name
+}
+
+output "mitnet_alb_restricted_sgid" {
+  description = "Restricted ALB security group ID"
+  value       = module.mitnet_alb_restricted.security_group_id
+}
+
+output "mitnet_alb_restricted_default_target_group_arn" {
+  description = "Restricted ALB default target group arn"
+  value       = module.mitnet_alb_restricted.default_target_group_arn
+}
+
+output "mitnet_alb_restricted_http_listener_arn" {
+  description = "Restricted ALB HTTP listener ARN"
+  value       = module.mitnet_alb_restricted.http_listener_arn
+}
+
+output "mitnet_alb_restricted_https_listener_arn" {
+  description = "Restricted ALB HTTPS listener ARN"
+  value       = module.mitnet_alb_restricted.https_listener_arn
+}
+
+output "mitnet_alb_restricted_all_ingress_sgid" {
+  description = "Restricted ALB security group ID allowing all ingress traffic from ALB"
+  value       = module.mitnet_all_access_from_alb.this_security_group_id
+}
+
+output "mitnet_alb_restricted_zone_id" {
+  description = "Restricted ALB zone id"
+  value       = module.mitnet_alb_restricted.alb_zone_id
+}
+
+# MITnet Public
+output "mitnet_alb_public_arn" {
+  description = "Public ALB arn"
+  value       = module.mitnet_alb_public.alb_arn
+}
+
+output "mitnet_alb_public_arn_suffix" {
+  description = "The ARN suffix of the ALB"
+  value       = module.mitnet_alb_public.alb_arn_suffix
+}
+
+output "mitnet_alb_public_name" {
+  description = "Public ALB name"
+  value       = module.mitnet_alb_public.alb_name
+}
+
+output "mitnet_alb_public_dnsname" {
+  description = "Public ALB DNS name"
+  value       = module.mitnet_alb_public.alb_dns_name
+}
+
+output "mitnet_alb_public_sgid" {
+  description = "Public ALB security group ID"
+  value       = module.mitnet_alb_public.security_group_id
+}
+
+output "mitnet_alb_public_default_target_group_arn" {
+  description = "Public ALB default target group arn"
+  value       = module.mitnet_alb_public.default_target_group_arn
+}
+
+output "mitnet_alb_public_http_listener_arn" {
+  description = "Public ALB HTTP listener ARN"
+  value       = module.mitnet_alb_public.http_listener_arn
+}
+
+output "mitnet_alb_public_https_listener_arn" {
+  description = "Public ALB HTTPS listener ARN"
+  value       = module.mitnet_alb_public.https_listener_arn
+}
+
+output "mitnet_alb_public_all_ingress_sgid" {
+  description = "Public ALB security group ID allowing all ingress traffic from ALB"
+  value       = module.mitnet_all_access_from_alb_public.this_security_group_id
+}
+
+output "mitnet_alb_public_zone_id" {
+  description = "Public ALB zone id"
+  value       = module.mitnet_alb_public.alb_zone_id
+}
