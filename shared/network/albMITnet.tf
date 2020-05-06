870e3f76460bb0048389572553d7f96a2052c859
eldamit <61520319+eldamit@users.noreply.github.com>
2020-05-06 19:59:51 -0400

    alb update

--- shared/network/albMITnet.tf
+++ shared/network/albMITnet.tf
@@ -3,43 +3,39 @@
 ######################
 #Creates default restricted (18.0.0.0/9) ALB that uses *.mitlib.net cert by default
 module "mitnet_alb_restricted" {
-  source                    = "github.com/mitlibraries/tf-mod-alb?ref=0.11"
-  # name                      = "${module.label.name}-alb-restricted"
+  source                    = "github.com/mitlibraries/tf-mod-alb?ref=0.12"
   name                      = "mitnet-alb-restricted"
-  http_ingress_cidr_blocks  = ["${var.alb_cidrs}"]
-  https_ingress_cidr_blocks = ["${var.alb_cidrs}"]
-  vpc_id = var.vpc_id
-  # vpc_id                    = "${module.vpc.vpc_id}"
+  http_ingress_cidr_blocks  = var.alb_cidrs
+  https_ingress_cidr_blocks = var.alb_cidrs
+  vpc_id                    = var.vpc_id
   ip_address_type           = "ipv4"
   subnet_ids                = var.public_subnets
-  # subnet_ids                = ["${module.vpc.public_subnets}"]
   access_logs_enabled       = "false"
-  access_logs_region        = "${var.aws_region}"
+  access_logs_region        = var.aws_region
   https_enabled             = "true"
-  certificate_arn           = "${module.shared.mitlib_cert}"
+  certificate_arn           = module.shared.mitlib_cert
 }
 
-module "sg_label" {
-  source = "github.com/mitlibraries/tf-mod-name?ref=0.11"
+module "mitnet_sg_label" {
+  source = "github.com/mitlibraries/tf-mod-name?ref=0.12"
   name   = "mitnet-alb-restricted"
 }
 
 # Create default security group to allow all ingress from restricted ALB
 module "mitnet_all_access_from_alb" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "2.13.0"
+  version = "3.8.0"
 
   name        = "${module.mitnet_alb_restricted.alb_name} all Ingress"
   description = "Allow all ingress from restricted ALB"
   vpc_id      = var.vpc_id
-  # vpc_id      = "${module.shared.vpc_id}" 
 
   computed_ingress_with_source_security_group_id = [
     {
       from_port                = 0
       to_port                  = 0
       protocol                 = "-1"
-      source_security_group_id = "${module.mitnet_alb_restricted.security_group_id}"
+      source_security_group_id = module.mitnet_alb_restricted.security_group_id
     },
   ]
 
@@ -52,21 +48,19 @@ module "mitnet_all_access_from_alb" {
 #########################
 #Creates default public ALB that uses *.mitlib.net cert by default
 module "mitnet_alb_public" {
-  source              = "github.com/mitlibraries/tf-mod-alb?ref=0.11"
+  source              = "github.com/mitlibraries/tf-mod-alb?ref=0.12"
   name                = "mitnet-alb-public"
   vpc_id              = var.vpc_id
-  # vpc_id              = "${module.vpc.vpc_id}"
   ip_address_type     = "ipv4"
-  subnet_ids                = var.public_subnets
-  # subnet_ids          = ["${module.vpc.public_subnets}"]
+  subnet_ids          = var.public_subnets
   access_logs_enabled = "false"
-  access_logs_region  = "${var.aws_region}"
+  access_logs_region  = var.aws_region
   https_enabled       = "true"
-  certificate_arn     = "${module.shared.mitlib_cert}"
+  certificate_arn     = module.shared.mitlib_cert
 }
 
-module "sg_label_public" {
-  source = "github.com/mitlibraries/tf-mod-name?ref=0.11"
+module "mitnet_sg_label_public" {
+  source = "github.com/mitlibraries/tf-mod-name?ref=0.12"
   name   = "mitnet-alb-public"
 }
 
@@ -74,24 +68,23 @@ module "sg_label_public" {
 
 module "all_access_from_mitnet_alb_public" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "2.13.0"
+  version = "3.8.0"
 
   name        = "${module.mitnet_alb_public.alb_name} all Ingress"
   description = "Allow all ingress from restricted ALB"
-  vpc_id              = var.vpc_id
-  # vpc_id      = "${module.shared.vpc_id}"
+  vpc_id      = var.vpc_id
 
   computed_ingress_with_source_security_group_id = [
     {
       from_port                = 0
       to_port                  = 0
       protocol                 = "-1"
-      source_security_group_id = "${module.mitnet_alb_public.security_group_id}"
+      source_security_group_id = module.mitnet_alb_public.security_group_id
     },
   ]
 
   number_of_computed_ingress_with_source_security_group_id = 1
-  tags                                                     = "${module.sg_label_public.tags}"
+  tags                                                     = module.sg_label_public.tags
 }
 
 
