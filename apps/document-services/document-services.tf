c0aaa252ce5cbcff73143d27002fea82cdb7c68c
V. Alex Brennen <vab@protonmail.com>
2020-07-01 07:44:05 -0400

    TF v0.12 update; disable rolling updates; code clean up; doc updates

--- apps/document-services/document-services.tf
+++ apps/document-services/document-services.tf
@@ -1,11 +1,11 @@
 module "label" {
-  source = "github.com/mitlibraries/tf-mod-name?ref=0.11"
+  source = "github.com/mitlibraries/tf-mod-name?ref=0.12"
   name   = "document-services"
 }
 
 resource "aws_security_group" "default" {
   description = "${module.label.name} Additional Security Groups"
-  vpc_id      = "${module.shared.vpc_id}"
+  vpc_id      = module.shared.vpc_id
   name        = "${module.label.name}-additional"
 
   ingress {
@@ -22,65 +22,65 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = "${module.label.tags}"
+  tags = module.label.tags
 }
 
 # Create an S3 Bucket to store MIT cert and key, and attach to beanstalk instance profile for access
-
 module "s3_cert_store" {
-  source             = "github.com/mitlibraries/tf-mod-s3-iam?ref=0.11"
+  source             = "github.com/mitlibraries/tf-mod-s3-iam?ref=0.12"
   name               = "document-services-certstore"
   versioning_enabled = "false"
 }
 
 resource "aws_iam_role_policy_attachment" "default_ro" {
-  role       = "${module.eb_docsvcs.ec2_instance_profile_role_name}"
-  policy_arn = "${module.s3_cert_store.readonly_arn}"
+  role       = module.eb_docsvcs.ec2_instance_profile_role_name
+  policy_arn = module.s3_cert_store.readonly_arn
 }
 
 module "rds_docsvcs" {
-  source                      = "github.com/mitlibraries/tf-mod-rds?ref=0.11"
-  engine                      = "mysql"
-  engine_version              = "5.5.61"
-  instance_class              = "db.t3.micro"
-  allocated_storage           = 20
+  source                      = "github.com/mitlibraries/tf-mod-rds?ref=0.12"
   name                        = "docsvcs-rds"
-  database_name               = "docsvcs"
-  database_user               = "${var.rds_username}"
-  database_password           = "${var.rds_password}"
-  database_port               = "3306"
-  db_parameter_group          = "mysql5.5"
-  maintenance_window          = "Sun:00:00-Sun:03:00"
-  backup_window               = "03:00-06:00"
-  backup_retention_period     = 30
-  vpc_id                      = "${module.shared.vpc_id}"
-  subnet_ids                  = ["${module.shared.private_subnets}"]
-  security_group_ids          = ["${module.eb_docsvcs.security_group_id}"]
-  major_engine_version        = "5.5"
-  allow_major_version_upgrade = "false"
-  apply_immediately           = "true"
-  dns_zone_id                 = "${module.shared.private_zoneid}"
+  engine                      = var.rds_engine
+  engine_version              = var.rds_maj_eng_ver
+  instance_class              = var.rds_inst_class
+  allocated_storage           = var.rds_storage
+  database_name               = var.rds_db_name
+  database_user               = var.rds_username
+  database_password           = var.rds_password
+  database_port               = var.rds_port
+  db_parameter_group          = var.rds_param_grp
+  maintenance_window          = var.rds_maint_win
+  backup_window               = var.rds_backup_win
+  backup_retention_period     = var.rds_backup_retain
+  vpc_id                      = module.shared.vpc_id
+  subnet_ids                  = module.shared.private_subnets
+  security_group_ids          = [module.eb_docsvcs.security_group_id]
+  major_engine_version        = var.rds_eng_ver
+  allow_major_version_upgrade = var.rds_maj_upgrade
+  apply_immediately           = var.rds_apply_immediately
+  dns_zone_id                 = module.shared.private_zoneid
 }
 
 module "eb_docsvcs" {
-  source = "github.com/mitlibraries/tf-mod-elasticbeanstalk-env?ref=0.11"
+  source = "github.com/mitlibraries/tf-mod-elasticbeanstalk-env?ref=0.12.1"
 
-  app     = "${module.shared.docsvcs_app_name}"
-  keypair = "mit-mjbernha"
+  app     = module.shared.docsvcs_app_name
+  keypair = var.ssh_keypair
   name    = "document-services"
-  vpc_id  = "${module.shared.vpc_id}"
+  vpc_id  = module.shared.vpc_id
 
   # We use public_subnets here since it's a singleInstance that needs to be accessed publicly
-  instance_subnets            = ["${module.shared.public_subnets}"]
-  security_groups             = ["${aws_security_group.default.id}"]
-  instance_type               = "t3.nano"
+  instance_subnets            = module.shared.public_subnets
+  security_groups             = [aws_security_group.default.id]
+  instance_type               = var.eb_instance_class
   associate_public_ip_address = "true"
   environment_type            = "SingleInstance"
+  rolling_update_enabled      = "false"
   rolling_update_type         = "Time"
   updating_min_in_service     = "0"
-  solution_stack_name         = "64bit Amazon Linux 2018.03 v2.8.8 running PHP 5.4"
-  zone_id                     = "${module.shared.public_zoneid}"
-  ssh_source_restriction      = "18.0.0.0/9"
+  solution_stack_name         = var.eb_solution_stack
+  zone_id                     = module.shared.public_zoneid
+  ssh_source_restriction      = var.ssh_subnet_restriction
   enable_managed_actions      = "false"
   autoscale_min               = "1"
   autoscale_max               = "1"
@@ -89,24 +89,24 @@ module "eb_docsvcs" {
   document_root = "/"
 
   # Environment Variables
-  # BUCKET_ID and *_S3 Variables are used for SSL config and are deployed via .ebxtensions in the app
+  # BUCKET_ID and *_S3 Variables are used for TLS config and are deployed via .ebxtensions in the app
   env_vars = "${
     map(
       "RDS_HOSTNAME", "${join(",", module.rds_docsvcs.hostname)}",
-      "RDS_USERNAME", "${var.rds_username}",
-      "RDS_PASSWORD", "${var.rds_password}",
-      "RDS_DB_NAME", "docsvcs",
-      "ENVIRONMENT", "${terraform.workspace}",
-      "BUCKET_ID", "${module.s3_cert_store.bucket_id}",
+      "RDS_USERNAME", var.rds_username,
+      "RDS_PASSWORD", var.rds_password,
+      "RDS_DB_NAME", var.rds_db_name,
+      "ENVIRONMENT", terraform.workspace,
+      "BUCKET_ID", module.s3_cert_store.bucket_id,
       "INCOMMON_S3", "https://${module.s3_cert_store.bucket_domain_name}/InCommonChain.crt",
       "CERT_S3", "https://${module.s3_cert_store.bucket_domain_name}/${module.label.name}.mit.edu.crt",
       "KEY_S3", "https://${module.s3_cert_store.bucket_domain_name}/${module.label.name}.mit.edu.key",
-      "CYBERSOURCE_ACCESS_KEY", "${var.cybersource_access_key}",
-      "CYBERSOURCE_PROFILE_ID", "${var.cybersource_profile_id}",
-      "MAIL_HOST", "${var.mail_host}",
-      "MAIL_PASSWORD", "${var.mail_password}",
-      "MAIL_PORT", "${var.mail_port}",
-      "MAIL_USERNAME", "${var.mail_username}"
+      "CYBERSOURCE_ACCESS_KEY", var.cybersource_access_key,
+      "CYBERSOURCE_PROFILE_ID", var.cybersource_profile_id,
+      "MAIL_HOST", var.mail_host,
+      "MAIL_PASSWORD", var.mail_password,
+      "MAIL_PORT", var.mail_port,
+      "MAIL_USERNAME", var.mail_username
     )
   }"
 }
--- apps/document-services/variables.tf
+++ apps/document-services/variables.tf
@@ -1,43 +1,139 @@
-variable "rds_username" {
-  type        = "string"
-  default     = ""
-  description = "(Required unless a `snapshot_identifier` or `replicate_source_db` is provided) Username for the master DB user"
-}
-
-variable "rds_password" {
-  type        = "string"
-  default     = ""
-  description = "(Required unless a snapshot_identifier or replicate_source_db is provided) Password for the master DB user"
-}
-
+# MIT Merchant Services Integration Variables
 variable "cybersource_access_key" {
-  type        = "string"
-  default     = ""
+  type        = string
   description = "Identifier passed to MIT Merchant Services to ensure payments are classified correctly"
 }
 
 variable "cybersource_profile_id" {
-  type        = "string"
-  default     = ""
+  type        = string
   description = "Identifier passed to MIT Merchant Services to ensure payments are classified correctly"
 }
 
+# EB Variables
+variable "eb_instance_class" {
+  type        = string
+  description = "The class of the EB instance"
+}
+
+variable "eb_solution_stack" {
+  type        = string
+  description = "The solution stack used to to build the EB instance"
+}
+
+# MIT Email System Integration Variables
 variable "mail_host" {
-  type        = "string"
+  type        = string
   description = "Hostname of external SMTP email server"
 }
 
+variable "mail_username" {
+  type        = string
+  description = "Username associated with account used to send email"
+}
+
 variable "mail_password" {
-  type        = "string"
+  type        = string
   description = "Password associated with account used to send email"
 }
 
 variable "mail_port" {
-  type        = "string"
+  type        = string
   description = "Port used to connect to external SMTP email server"
 }
 
-variable "mail_username" {
-  type        = "string"
-  description = "Username associated with account used to send email"
+# RDS resource module related variables
+variable "rds_engine" {
+  type        = string
+  description = "RDS database engine"
+}
+
+variable "rds_maj_eng_ver" {
+  type        = string
+  description = "RDS database engine major version"
+}
+
+variable "rds_eng_ver" {
+  type        = string
+  description = "RDS database engine version"
+}
+
+variable "rds_maj_upgrade" {
+  type        = string
+  description = "Allow automated RDS database major engine version upgrade"
+}
+
+variable "rds_apply_immediately" {
+  type        = string
+  description = "Apply database modifications immediately or during next maintenance window"
+}
+
+variable "rds_inst_class" {
+  type        = string
+  description = "Class of the RDS instance"
+}
+
+variable "rds_storage" {
+  type        = string
+  description = "The amount of storage for the RDS instance in GB"
+}
+
+variable "rds_db_name" {
+  type        = string
+  description = "The RDS database name"
+}
+
+variable "rds_port" {
+  type        = string
+  description = "The TCP port for the RDS instance to use"
+}
+
+variable "rds_param_grp" {
+  type        = string
+  default     = ""
+  description = "The RDS parameter group to use with the RDS database"
+}
+
+variable "rds_maint_win" {
+  type        = string
+  default     = ""
+  description = "RDS maintenance window"
+}
+
+variable "rds_backup_win" {
+  type        = string
+  default     = ""
+  description = "RDS database backup window"
+}
+
+variable "rds_backup_retain" {
+  type        = string
+  default     = "0"
+  description = "RDS backup retention period"
+}
+
+variable "rds_username" {
+  type        = string
+  description = "Username for the master DB user"
+}
+
+variable "rds_password" {
+  type        = string
+  description = "Password for the master DB user"
+}
+
+variable "rds_subnets" {
+  type        = list(string)
+  default     = []
+  description = "Subnet to use for RDS DB"
+}
+
+# Shell Access Variables
+variable "ssh_keypair" {
+  type        = string
+  description = "SSH Keypair allowed shell login access"
+}
+
+variable "ssh_subnet_restriction" {
+  type        = string
+  description = "CIDR subnet mask for network allowed to connect over ssh"
 }
