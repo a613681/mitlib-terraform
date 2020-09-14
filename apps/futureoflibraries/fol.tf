ae4a3d1e0f6dca9a14c15f01908e35291cca2b81
V. Alex Brennen <vab@protonmail.com>
2020-09-14 10:08:00 -0400

    upgrade fol to TF v0.12

--- apps/futureoflibraries/fol.tf
+++ apps/futureoflibraries/fol.tf
@@ -2,22 +2,22 @@ resource "aws_route53_record" "fol" {
   name    = "future-of-libraries"
   ttl     = 300
   type    = "CNAME"
-  zone_id = "${module.shared.public_zoneid}"
-  records = ["live-mitlib-futureoflibraries.pantheonsite.io"]
+  zone_id = module.shared.public_zoneid
+  records = var.r53_fol_value
 }
 
 resource "aws_route53_record" "fol_dev" {
   name    = "future-of-libraries-dev"
   ttl     = 300
   type    = "CNAME"
-  zone_id = "${module.shared.public_zoneid}"
-  records = ["dev-mitlib-futureoflibraries.pantheonsite.io"]
+  zone_id = module.shared.public_zoneid
+  records = var.r53_fol_dev_value
 }
 
 resource "aws_route53_record" "fol_test" {
   name    = "future-of-libraries-test"
   ttl     = 300
   type    = "CNAME"
-  zone_id = "${module.shared.public_zoneid}"
-  records = ["test-mitlib-futureoflibraries.pantheonsite.io"]
+  zone_id = module.shared.public_zoneid
+  records = var.r53_fol_test_value
 }
